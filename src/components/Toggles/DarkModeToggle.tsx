1014b75b752556f785ff0985e776d785345faa22
Rogelio Rios <rogeliorios348@gmail.com>
2022-07-07 03:41:31 -0500

    Dynamic Dark/Light Toggle

--- src/components/Toggles/DarkModeToggle.tsx
+++ src/components/Toggles/DarkModeToggle.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Flex, IconButton, Tooltip, useColorMode } from "@chakra-ui/react";
-import { CgDarkMode } from 'react-icons/cg'
+import { CgDarkMode } from 'react-icons/cg';
+import { MdLightMode, MdDarkMode } from 'react-icons/md'
 
 const DarkModeToggle = () => {
     const {colorMode, toggleColorMode } = useColorMode();[]
   return (
     <Flex align='center' justify='center' height='4rem' direction='column'>
-        <Tooltip label='Toggle Dark Mode'>
-            <IconButton onClick={() => toggleColorMode()} fontSize='35px' colorScheme='yellow' variant='ghost' aria-label='Toggle Dark Mode' icon={<CgDarkMode />}/>
+        <Tooltip label={colorMode === 'light' ? 'Toggle Dark Mode' : 'Toggle Light Mode'}>
+            <IconButton onClick={() => toggleColorMode()} fontSize='35px' colorScheme={colorMode === 'light' ? 'yellow' : 'yellow'} variant='ghost' aria-label='Toggle Dark Mode' icon={colorMode === 'light' ? <MdDarkMode />: <MdLightMode/> }/>
         </Tooltip>
     </Flex>
   )
